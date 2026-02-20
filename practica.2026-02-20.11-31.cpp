6152e3d3affa193b72f45ccea01b990829a06a16
fabian222005 <fabian222005@yahoo.com.mx>
2026-02-20 11:35:46 -0600

    Update 2026-02-20

---
+++ practica.2026-02-20.11-31.cpp
@@ -0,0 +1,28 @@
+
+/************************
+Instituto Tecnologico de Durango
+Departamento Metal-Mecanica
+
+Programacion Avanzada 7U
+Fabian Quiñones Escarzaga 23040871
+Ing. Mario Gerardo Hernandez Marines
+2026-02-20.11-31
+Practica clasesHeredadasconConstructor
+************************/
+#include <iostream>
+class A {
+public:
+A(int argA){
+std::cout<<"Hola desde A\n";
+a=argA;
+}
+int getA(){return(A);}
+private:
+int argA;
+};
+
+int main(){
+A testA(10);
+testA.getA();
+
+}
